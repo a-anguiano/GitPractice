0e56f33ab5a41e3f39718950c3621b46214df373
Allison Anguiano <aanguiano@dev-10.com>
2022-03-09 14:43:35 -0600

    World! to Changed

--- PracticeGit/Program.cs
+++ PracticeGit/Program.cs
@@ -6,7 +6,7 @@ namespace PracticeGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Changed");
         }
     }
 }
